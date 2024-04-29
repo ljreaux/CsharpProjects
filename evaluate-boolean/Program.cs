d1bf682c204965f47ca7d8c2d1053053b8de7d41
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-04-29 15:19:39 -0500

    added new project

---
+++ evaluate-boolean/Program.cs
@@ -0,0 +1,22 @@
+﻿string permission = "Manager";
+int level = 18;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager") && level >= 20)
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
