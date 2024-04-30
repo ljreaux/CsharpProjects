dd6cb56e88b476ce634a5319b90819ee4d6e2f3c
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-04-30 14:40:45 -0500

    finished for unit

---
+++ for/Program.cs
@@ -0,0 +1,16 @@
+﻿// fizbuzz problem
+// val / 3 Fizz
+// val / 5 Buzz
+// val / 3 && val / 5 FizzBuzz
+
+for (int i = 1; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else
+        Console.WriteLine(i);
+}
