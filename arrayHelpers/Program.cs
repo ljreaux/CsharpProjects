cfd250972cc58375ede40b3d9cd309da3c14a104
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-05-30 18:01:22 -0500

    finished several projects

--- arrayHelpers/Program.cs
+++ arrayHelpers/Program.cs
@@ -1,2 +1,93 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string[] pallets = { "B14", "A11", "B12", "A13" };
+
+// // Console.WriteLine("Sorted...");
+// // Array.Sort(pallets);
+// // foreach (var pallet in pallets)
+// // {
+// //     Console.WriteLine($"-- {pallet}");
+// // }
+
+// // Console.WriteLine("");
+// // Console.WriteLine("Reversed...");
+// // Array.Reverse(pallets);
+// // foreach (var pallet in pallets)
+// // {
+// //     Console.WriteLine($"-- {pallet}");
+// // }
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+// Console.WriteLine(result);
+
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+// string pangram = "The quick brown fox jumps over the lazy dog";
+// string result = "";
+// string[] words = pangram.Split(' ');
+
+// for (int i = 0; i < words.Length; i++)
+// {
+//     string word = words[i];
+//     char[] currentWord = word.ToCharArray();
+//     Array.Reverse(currentWord);
+//     string reversedWord = new string(currentWord);
+//     words[i] = reversedWord;
+// }
+
+// result = String.Join(" ", words);
+
+// Console.WriteLine(result);
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orders = orderStream.Split(',');
+
+foreach (string order in orders)
+{
+    string displayString;
+    if (order.Length == 4)
+    {
+        displayString = order;
+    }
+    else
+    {
+        displayString = $"{order}\t- Error";
+    }
+    Console.WriteLine(displayString);
+}
---
+++ stringHelpers/Program.cs
@@ -0,0 +1,123 @@
+﻿// string message = "What is the value <span>between the tags</span>?";
+
+// const string openSpan = "<span>";
+// const string closeSpan = "</span>";
+
+// int openingPosition = message.IndexOf(openSpan);
+// int closingPosition = message.IndexOf(closeSpan);
+
+// openingPosition += openSpan.Length;
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+// string message = "(What if) there are (more than) one (set of parentheses)?";
+// while (true)
+// {
+//     int openingPosition = message.IndexOf('(');
+//     if (openingPosition == -1)
+//         break;
+
+//     openingPosition += 1;
+//     int closingPosition = message.IndexOf(')');
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+
+//     // Note the overload of the Substring to return only the remaining
+//     // unprocessed message:
+//     message = message.Substring(closingPosition + 1);
+// }
+
+// string message =
+//     "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// // The IndexOfAny() helper method requires a char array of characters.
+// // You want to look for:
+
+// char[] openSymbols = { '[', '{', '(' };
+
+// // You'll use a slightly different technique for iterating through
+// // the characters in the string. This time, use the closing
+// // position of the previous iteration as the starting index for the
+// //next open symbol. So, you need to initialize the closingPosition
+// // variable to zero:
+
+// int closingPosition = 0;
+
+// while (true)
+// {
+//     int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+//     if (openingPosition == -1)
+//         break;
+
+//     string currentSymbol = message.Substring(openingPosition, 1);
+
+//     // Now  find the matching closing symbol
+//     char matchingSymbol = ' ';
+
+//     switch (currentSymbol)
+//     {
+//         case "[":
+//             matchingSymbol = ']';
+//             break;
+//         case "{":
+//             matchingSymbol = '}';
+//             break;
+//         case "(":
+//             matchingSymbol = ')';
+//             break;
+//     }
+
+//     // To find the closingPosition, use an overload of the IndexOf method to specify
+//     // that the search for the matchingSymbol should start at the openingPosition in the string.
+
+//     openingPosition += 1;
+//     closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+//     // Finally, use the techniques you've already learned to display the sub-string:
+
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+// }
+
+// string data = "12345John Smith          5000  3  ";
+// string updatedData = data.Remove(5, 20);
+// Console.WriteLine(updatedData);
+
+// string message = "This--is--ex-amp-le--da-ta";
+// message = message.Replace("--", " ");
+// message = message.Replace("-", "");
+// Console.WriteLine(message);
+
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+quantity = "Quantity: ";
+output = "Output: ";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+const string openDiv = "<div>";
+const string closeDiv = "</div>";
+
+int openingQuantPosition = input.IndexOf(openSpan);
+openingQuantPosition += openSpan.Length;
+int closingQuantPosition = input.IndexOf(closeSpan);
+int quantLength = closingQuantPosition - openingQuantPosition;
+
+quantity += input.Substring(openingQuantPosition, quantLength);
+
+int openingOutputPosition = input.IndexOf(openDiv);
+openingOutputPosition += openDiv.Length;
+int closingOutputPosition = input.IndexOf(closeDiv);
+int outputLength = closingOutputPosition - openingOutputPosition;
+
+output += input.Substring(openingOutputPosition, outputLength);
+output = output.Replace("&trade;", "&reg;");
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
